17e5196a31e41c30f2840bfb9f99d4b032f0e347
lalit yogi <yogi.lalit2391@gmail.com>
2020-05-22 16:41:38 +0530

    remove unnecessary message

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('cleint side javascript is running')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const errp = document.querySelector('#errp')
